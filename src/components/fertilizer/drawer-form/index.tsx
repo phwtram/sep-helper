4eae75b9a250c8dbf693982fb8016fc9d4fa12d9
quocviet064 <thqviet12c5.ncthanh@gmail.com>
2025-02-27 14:07:20 +0700

    fix(ui): fix button alignment issue

--- src/components/fertilizer/drawer-form/index.tsx
+++ src/components/fertilizer/drawer-form/index.tsx
@@ -24,6 +24,7 @@ import { Drawer } from "../../drawer";
 import { UploadOutlined } from "@ant-design/icons";
 import { useStyles } from "./styled";
 import { IFertilizer } from "@/interfaces";
+import { UploadFile } from "antd/lib";
 
 type Props = {
   id?: BaseKey;
@@ -68,6 +69,17 @@ export const FertilizerDrawerForm = (props: Props) => {
     });
   };
 
+  const normalizeImage = (image: any) => {
+    if (!image) return [];
+    if (Array.isArray(image)) return image;
+    return [{ url: image }];
+  };
+
+  const initialValues = {
+    ...formProps.initialValues,
+    image: normalizeImage(formProps.initialValues?.image),
+  };
+
   const image = Form.useWatch("image", formProps.form);
   const title = props.action === "edit" ? "Edit Fertilizer" : "Add Fertilizer";
 
@@ -98,11 +110,19 @@ export const FertilizerDrawerForm = (props: Props) => {
       onClose={onDrawerClose}
     >
       <Spin spinning={formLoading}>
-        <Form {...formProps} layout="vertical">
+        <Form {...formProps} initialValues={initialValues} layout="vertical">
           <Form.Item
             name="image"
             valuePropName="fileList"
-            getValueFromEvent={getValueFromEvent}
+            getValueFromEvent={(e) => {
+              if (!e || !e.fileList) return [];
+              return e.fileList.map((file: UploadFile) => ({
+                url: file.response?.url || file.url,
+                name: file.name,
+                uid: file.uid,
+                originFileObj: file.originFileObj,
+              }));
+            }}
             style={{ margin: 0 }}
             rules={[{ required: true }]}
           >
@@ -112,7 +132,7 @@ export const FertilizerDrawerForm = (props: Props) => {
               maxCount={1}
               accept=".png,.jpg,.jpeg"
               className={styles.uploadDragger}
-              showUploadList={false}
+              showUploadList={true}
             >
               <Flex
                 vertical
@@ -125,13 +145,17 @@ export const FertilizerDrawerForm = (props: Props) => {
                   style={{
                     aspectRatio: 1,
                     objectFit: "contain",
-                    width: image ? "100%" : "48px",
-                    height: image ? "100%" : "48px",
-                    marginTop: image ? undefined : "auto",
-                    transform: image ? undefined : "translateY(50%)",
+                    width: image?.length ? "100%" : "48px",
+                    height: image?.length ? "100%" : "48px",
+                    marginTop: image?.length ? undefined : "auto",
+                    transform: image?.length ? undefined : "translateY(50%)",
                   }}
-                  src={image || "/images/fertilizer-default-img.png"}
-                  alt="Fertilizer Image"
+                  src={
+                    image?.length > 0
+                      ? image[0]?.url || "/images/item-default-img.png"
+                      : "/images/item-default-img.png"
+                  }
+                  alt="Item Image"
                 />
                 <Button
                   icon={<UploadOutlined />}
@@ -213,7 +237,12 @@ export const FertilizerDrawerForm = (props: Props) => {
               style={{ padding: "16px 16px 0px 16px" }}
             >
               <Button onClick={onDrawerClose}>Cancel</Button>
-              <SaveButton {...saveButtonProps} htmlType="submit" type="primary" icon={null}>
+              <SaveButton
+                {...saveButtonProps}
+                htmlType="submit"
+                type="primary"
+                icon={null}
+              >
                 Save
               </SaveButton>
             </Flex>
